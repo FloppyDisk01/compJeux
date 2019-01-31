962f684a6fc96427df9148d8acdddc8f67d17f27
FloppyDisk01 <aurelien.filatre@gmail.com>
2019-01-31 15:44:14 +0100

    debut projet

---
+++ src/Comparateur/Comparateur.java
@@ -0,0 +1,20 @@
+package Comparateur;
+
+public class Comparateur {
+	private String name;
+	private int note;
+	
+	public Comparateur(String nameSearch, int scoreSearch){
+		this.name = nameSearch;
+		this.note = scoreSearch;
+		
+	}
+	
+	public void interrogateDB() {
+		
+	}
+	
+	public void hierarchyDB() {
+		
+	}
+}
