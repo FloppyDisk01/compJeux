19382b755aa4daf5e95323cff432bf3dd28e7ea3
FloppyDisk01 <aurelien.filatre@gmail.com>
2019-02-06 00:51:50 +0100

    ajout sous classes Jeu  (genre/plateforme/editeur) ver1.0

--- src/Jeu/Genre.java
+++ src/Jeu/Genre.java
@@ -1,9 +1,21 @@
 package Jeu;
 
+import java.util.ArrayList;
+
 public class Genre {
+	private ArrayList<Genre> listeGenre;
 	private String nGenre;
 	
 	public Genre(String nomG) {
 		this.nGenre = nomG;
+	}
+	
+	/**
+	 * remplis listeGenre grace a genreModele
+	 * @return liste complète des genres
+	 */
+	public ArrayList<Genre> getAllGenre() {
+		//TODO
+		return listeGenre;
 	}
 }
--- src/Jeu/Plateforme.java
+++ src/Jeu/Plateforme.java
@@ -1,9 +1,28 @@
 package Jeu;
 
+import java.util.ArrayList;
+
 public class Plateforme {
+	private ArrayList<Plateforme> listePlateforme;
 	private String nPlateforme;
 	
 	public Plateforme(String nomP) {
 		this.nPlateforme = nomP;
+	}
+	
+	/**
+	 * remplis listePlateforme grace a plateformeModele
+	 * @return liste complète des plateformes
+	 */
+	public ArrayList<Plateforme> getAllPlateforme() {
+		//TODO
+		return listePlateforme;
+	}
+	
+	/**
+	 * ajout de la plateforme à la BDD s'il n'est pas déjà présent.
+	 */
+	public void ajoutBDD() {
+		//TODO
 	}
 }
