4d29c8587e40b0f27843846db98c4fd29819b5c9
Léo Jean <leojlv34@gmail.com>
2019-02-05 19:13:15 +0100

    reste

--- src/BD/ConnexionBD.java
+++ src/BD/ConnexionBD.java
@@ -1,5 +1,46 @@
 package BD;
 
-public class ConnexionBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import Jeu.Jeu_video;
+
+public class ConnexionBD {
+	
+	
+	public static void ajoutJV() {
+		String url ="jdbc:mysql://mysql-ltpgame.alwaysdata.net/ltpgame_bd_projet";
+		String login = "ltpgame_";
+		String passwd = "projet4A";
+		Connection cn=null;
+		Statement st=null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn=DriverManager.getConnection(url, login, passwd);
+			st=cn.createStatement();
+			String sql="INSERT INTO Jeu_video VALUES ('1','a',30.0,'aventure','nintendo','ps4','1')";
+			st.executeUpdate(sql);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				cn.close();
+				st.close();
+			
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ajoutJV();
+	}
 }
--- src/Comparateur/Comparateur.java
+++ src/Comparateur/Comparateur.java
@@ -7,9 +7,17 @@ import Jeu.Jeu_video;
 public abstract class Comparateur implements Comparator<Jeu_video>{
 	private Jeu_video jv_recherche;
 	public int comparer(Jeu_video jv) {
-		return 0;
+		int score=0;
+		
+		if(this.jv_recherche.getNom()== jv.getNom()) {
+			score=score+0;
+		}
+		
+		
+		
+		return score;
 	}
 	public int compare(Jeu_video jv1,Jeu_video jv2) {
-		return 0;
+		return comparer(jv1)-comparer(jv2);
 	}
 }
