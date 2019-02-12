f7dfb0f2960704aeb2d4a3a97e81f6d8983958cf
Léo Jean <leojlv34@gmail.com>
2019-02-12 09:08:40 +0100

    ajout url dans modif et ajout admin

--- src/IHM/PAjout_jeu_admin.java
+++ src/IHM/PAjout_jeu_admin.java
@@ -22,22 +22,22 @@ public class PAjout_jeu_admin extends JPanel{
 	private JTextField jtf_prix;
 	
 	private JLabel lab_genre;
-	private JComboBox<String> list_genre;
 	private JTextField jtf_genre;
 	
 	private JLabel lab_editeur;
-	private JComboBox<String> list_editeur;
 	private JTextField jtf_editeur;
 	
 	private JLabel lab_plateforme;
-	private JComboBox<String> list_plateforme;
 	private JTextField jtf_plateforme;
 	
 	private JLabel lab_date;
 	private JTextField jtf_date;
 	
 	private JLabel lab_note;
-	private JComboBox<String> list_note;
+	private JTextField jtf_note;
+	
+	private JLabel lab_url;
+	private JTextField jtf_url;
 	
 	public PAjout_jeu_admin() {
 		bvalider=new JButton("Valider");
@@ -53,23 +53,21 @@ public class PAjout_jeu_admin extends JPanel{
 		jtf_date=new JTextField();
 		
 		lab_genre=new JLabel("Genre : ");
-		String[] items_genre= {};
-		list_genre=new JComboBox<String>(items_genre);
+		
 		jtf_genre=new JTextField();
 		
 		lab_editeur=new JLabel("Editeur : ");
-		String[] items_editeur= {};
-		list_editeur=new JComboBox<String>(items_editeur);
+		
 		jtf_editeur=new JTextField();
 		
 		lab_plateforme=new JLabel("Plateforme : ");
-		String[] items_plateforme= {};
-		list_plateforme=new JComboBox<String>(items_plateforme);
+		
 		jtf_plateforme=new JTextField();
 		
 		lab_note=new JLabel("Note : ");
-		String[] items_note= {};
-		list_note=new JComboBox<String>(items_note);
+		jtf_note=new JTextField();
+		
+		
 		
 		this.add(lab_nom);
 		this.add(jtf_nom);
@@ -84,22 +82,26 @@ public class PAjout_jeu_admin extends JPanel{
 		jtf_date.setPreferredSize(new Dimension(40,20));
 		
 		this.add(lab_genre);
-		this.add(list_genre);
 		this.add(jtf_genre);
 		jtf_genre.setPreferredSize(new Dimension(100,20));
 		
 		this.add(lab_editeur);
-		this.add(list_editeur);
+		
 		this.add(jtf_editeur);
 		jtf_editeur.setPreferredSize(new Dimension(100,20));
 		
 		this.add(lab_plateforme);
-		this.add(list_plateforme);
+		
 		this.add(jtf_plateforme);
 		jtf_plateforme.setPreferredSize(new Dimension(100,20));
 		
 		this.add(lab_note);
-		this.add(list_note);
+		this.add(jtf_note);
+		jtf_plateforme.setPreferredSize(new Dimension(40,20));
+		
+		this.add(lab_url);
+		this.add(jtf_url);
+		jtf_plateforme.setPreferredSize(new Dimension(100,20));
 		
 		this.add(bannuler);
 		this.add(bvalider);
